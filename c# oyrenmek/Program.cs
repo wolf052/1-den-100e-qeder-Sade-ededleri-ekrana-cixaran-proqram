63627e55e6e51ce6614cb97dd9585e1c0ca8bf2b
wolf052 <cmahur108@gmail.com>
2025-08-02 20:59:03 +0400

    1-den 100e qeder Sade ededleri ekrana cixaran proqram

---
+++ c# oyrenmek/Program.cs
@@ -0,0 +1,19 @@
+﻿for (int n = 2; n <= 100; n++)
+{
+    int i = 2;
+    int bolenSayi = 0;
+
+    while (i * i <= n)
+    {
+        if (n % i == 0)
+        {
+            bolenSayi = bolenSayi + 1;
+        }
+        i = i + 1;
+    }
+
+    if (bolenSayi == 0)
+    {
+        Console.WriteLine(n);
+    }
+}    
